3243b40d595bbc3771f0ba1487eb9de013c396a7
GREATPERSON <168116588+greatpersonxd@users.noreply.github.com>
2024-05-04 08:58:59 +0530

    Update tagall.py

--- MukeshRobot/modules/tagall.py
+++ MukeshRobot/modules/tagall.py
@@ -9,9 +9,9 @@ from MukeshRobot import telethn as client
 
 spam_chats = []
 
-@client.on(events.NewMessage(pattern="^@tagall ?(.*)"))
+@client.on(events.NewMessage(pattern="^@mall ?(.*)"))
 @client.on(events.NewMessage(pattern="^@all ?(.*)"))
-@client.on(events.NewMessage(pattern="^/tagall ?(.*)"))
+@client.on(events.NewMessage(pattern="^/mall ?(.*)"))
 @client.on(events.NewMessage(pattern="^@mention ?(.*)"))
 async def mentionall(event):
     chat_id = event.chat_id
