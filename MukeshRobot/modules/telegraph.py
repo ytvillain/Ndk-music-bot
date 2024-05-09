e2d27cfc8da45805b4b161ea216498da44ad4a2e
GREATPERSON <168116588+greatpersonxd@users.noreply.github.com>
2024-05-09 10:07:03 +0530

    Update telegraph.py

--- MukeshRobot/modules/telegraph.py
+++ MukeshRobot/modules/telegraph.py
@@ -14,7 +14,7 @@ r = telegraph.create_account(short_name=Mukesh)
 auth_url = r["auth_url"]
 
 
-@register(pattern="^/tg(m|t) ?(.*)")
+@register(pattern="^/mtg(m|t) ?(.*)")
 async def _(event):
     if event.fwd_from:
         return
